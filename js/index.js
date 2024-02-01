7eef4bc183b06997c67123bed80473730bb9f99e
Roman Khassazhirov <roman.khassazhirov@gmail.com>
2024-02-01 18:48:21 +0300

    Added the weather widget

--- js/index.js
+++ js/index.js
@@ -5,7 +5,14 @@ const time = document.querySelector('.time'),
   greeting = document.querySelector('.greeting'),
   yourName = document.querySelector('.name'),
   slideNext = document.querySelector('.slide-next'),
-  slidePrev = document.querySelector('.slide-prev');
+  slidePrev = document.querySelector('.slide-prev'),
+  weatherIcon = document.querySelector('.weather-icon'),
+  weatherDescription = document.querySelector('.weather-description'),
+  wind = document.querySelector('.wind'),
+  humidity = document.querySelector('.humidity'),
+  temperature = document.querySelector('.temperature'),
+  city = document.querySelector('.city'),
+  weatherErr = document.querySelector('.weather-error');
 
 let randomNum = 0,
   prevRandomNum = 0,
@@ -130,3 +137,55 @@ function getSlidePrev() {
 
 slideNext.addEventListener('click', getSlideNext);
 slidePrev.addEventListener('click', getSlidePrev);
+
+
+// 4. Виджет погоды
+async function getWeather() {
+  try {
+    weatherErr.textContent = '';
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value || getLS()}&lang=ru&appid=027d67b968453a2d261a28165e52007f&units=metric`;
+    let res = await fetch(url);
+    let data = await res.json();
+
+    weatherIcon.className = 'weather-icon owf'
+    weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+    temperature.textContent = `${Math.round(data.main.temp)}°C`;
+    weatherDescription.textContent = data.weather[0].description;
+    wind.textContent = `${data.wind.speed} м/с`;
+    humidity.textContent = `${data.main.humidity}%`;
+  } catch(e) {
+    console.log(e);
+    temperature.textContent = '';
+    weatherDescription.textContent = '';
+    weatherErr.textContent = 'Ооу, произошла ошибка. Проверьте правильность набранного вами города';
+  }
+
+}
+
+// Погода для определённого города
+function setWeather(event) {
+  if (event.code === 'Enter') {
+    city.addEventListener('change', getWeather);
+    city.blur();
+  }
+}
+
+document.addEventListener('DOMContentLoaded', getWeather);
+city.addEventListener('keypress', setWeather);
+
+// Localstorage для поля ввода города
+function setLS() {
+  localStorage.setItem('city', city.value);
+}
+
+window.addEventListener('beforeunload', setLS)
+
+function getLS() {
+  let cityName = localStorage.getItem('city');
+  if (cityName !== null) {
+    city.value = cityName;
+  }
+  return cityName;
+}
+
+window.addEventListener('load', getLS);
